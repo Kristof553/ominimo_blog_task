ac59517be1f9808c7a8712cfb31fcec296be0fbd
kristofmolnar553 <kristofmolnar553@gmail.com>
2025-09-18 19:15:55 +0200

    add comment migration and model

---
+++ backend/app/Models/Comment.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    use HasFactory;
+
+    protected $table = 'comments';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'post_id',
+        'user_id',
+        'comment',
+        'created_at',
+    ];
+
+    public function post(){
+        return $this->belongsTo(Post::class, 'post_id', 'id');
+    }
+
+    public function user(){
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+}
